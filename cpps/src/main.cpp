ca3cadf3df84073a78c7b5cb644f57dbcd9266e7
Seongho <bluetosh7@gmail.com>
2024-09-06 17:08:15 +0900

    feat: adjust main scene

--- cpps/src/main.cpp
+++ cpps/src/main.cpp
@@ -44,7 +44,7 @@ int main() {
 
   std::unique_ptr<PerspectiveCamera> camera =
       std::make_unique<PerspectiveCamera>(glm::radians(90.0f), aspect_ratio);
-  camera.get()->lookAt(glm::vec3(1, 1, 3), glm::vec3(0, 0, 0));
+  camera.get()->lookAt(glm::vec3(0.5, 0.5, 2), glm::vec3(0, 0, 0));
 
   std::unique_ptr<AmbientLight> ambient_light =
       std::make_unique<AmbientLight>(0.2f, glm::vec3(1.f, 1.f, 1.f));
@@ -55,13 +55,13 @@ int main() {
   Root root({width, height, *camera, *ambient_light, *directional_light});
   root.setClearColor(glm::vec4(0.1f, 0.1f, 0.1f, 1.0f));
 
-  std::string modelPath = "assets/models/cube.obj";
+  std::string holder_model_path = "assets/models/holder.obj";
 
-  std::unique_ptr<Geometry> cube_geometry =
-      std::make_unique<Geometry>(GeometryUtils::loadObjToGeometry(modelPath));
+  std::unique_ptr<Geometry> holder_geometry = std::make_unique<Geometry>(
+      GeometryUtils::loadObjToGeometry(holder_model_path));
+  std::unique_ptr<CubeGeometry> cube_geometry =
+      std::make_unique<CubeGeometry>(0.2f, 0.2f, 0.2f);
 
-  //   std::unique_ptr<CubeGeometry> cube_geometry =
-  //       std::make_unique<CubeGeometry>();
   std::unique_ptr<PlaneGeometry> plane_geometry =
       std::make_unique<PlaneGeometry>(8.0f, 8.0f);
 
@@ -70,6 +70,8 @@ int main() {
   std::unique_ptr<SingleColorMaterial> single_color_material =
       std::make_unique<SingleColorMaterial>();
 
+  std::unique_ptr<Mesh> holder =
+      std::make_unique<Mesh>(*holder_geometry, *phong_material);
   std::unique_ptr<Mesh> cube =
       std::make_unique<Mesh>(*cube_geometry, *phong_material);
 
@@ -77,10 +79,11 @@ int main() {
       std::make_unique<Mesh>(*plane_geometry, *single_color_material);
 
   plane.get()->rotate(glm::radians(-90.0f), glm::vec3(1, 0, 0));
-  plane.get()->translate(glm::vec3(0, -3, 0));
+  plane.get()->translate(glm::vec3(0, -2, 0));
+  holder.get()->translate(glm::vec3(0, -1, 0));
 
   std::unique_ptr<btCollisionShape> cube_collision_shape =
-      std::make_unique<btBoxShape>(btBoxShape(btVector3(0.5f, 0.5f, 0.5f)));
+      std::make_unique<btBoxShape>(btBoxShape(btVector3(0.1f, 0.1f, 0.1f)));
   std::unique_ptr<btMotionState> cube_motion_state =
       std::make_unique<btDefaultMotionState>(btDefaultMotionState(
           btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 0, 0))));
@@ -88,6 +91,16 @@ int main() {
       std::make_unique<PhysicsModule>(1.f, btVector3(0, 0, 0),
                                       std::move(cube_collision_shape),
                                       std::move(cube_motion_state));
+
+  std::unique_ptr<btCollisionShape> holder_collision_shape =
+      std::make_unique<btBoxShape>(btBoxShape(btVector3(0.5f, 0.5f, 0.5f)));
+  std::unique_ptr<btMotionState> holder_motion_state =
+      std::make_unique<btDefaultMotionState>(btDefaultMotionState(
+          btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 0, 0))));
+  std::unique_ptr<PhysicsModule> holder_physics_module =
+      std::make_unique<PhysicsModule>(1.f, btVector3(0, 0, 0),
+                                      std::move(holder_collision_shape),
+                                      std::move(holder_motion_state));
 
   // Plane Collision shape with width 8.0f and height 8.0f
   std::unique_ptr<btCollisionShape> plane_collision_shape =
@@ -104,12 +117,16 @@ int main() {
       std::make_unique<Entity>(std::move(cube), std::move(cube_physics_module));
   std::unique_ptr<Entity> plane_entity = std::make_unique<Entity>(
       std::move(plane), std::move(plane_physics_module));
+  std::unique_ptr<Entity> holder_entity = std::make_unique<Entity>(
+      std::move(holder), std::move(holder_physics_module));
 
   cube_entity.get()->syncPhysicsWithMesh();
   plane_entity.get()->syncPhysicsWithMesh();
+  holder_entity.get()->syncPhysicsWithMesh();
 
   root.scene_manager->addEntity(*cube_entity);
   root.scene_manager->addEntity(*plane_entity);
+  root.scene_manager->addEntity(*holder_entity);
 
   std::function<void(float, float)> loop_func = [&](float elapsed_ms,
                                                     float delta_ms) {
